7f74a19d94d408d4a862959b97c35902241d9116
Vicent Perez <vicennt@gmail.com>
2018-11-23 09:06:44 +0000

    First commit!

---
+++ SearchAlgorithms/src/com/vicennt/logic/SearchAlgorithms.java
@@ -0,0 +1,18 @@
+package com.vicennt.logic;
+
+/**
+ *
+ * @author vicent
+ */
+
+public class SearchAlgorithms {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
